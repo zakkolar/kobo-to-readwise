e8e3c4a9c5e0bb07802d32a0133dc430d77e5c03
Zak Kolar <zakkolar@users.noreply.github.com>
2022-01-17 21:18:29 -0500

    add .annot support

--- README.md
+++ README.md
@@ -14,8 +14,12 @@ Extract highlights from Kobo devices and put them into a CSV for Readwise
 4. Your highlights will be saved in `output.csv` in the same directory as the script
 
 ## Export notes
-- For each book, highlights will be exported in the order they were created rather than the order they appear in the book. This is due to the format Kobo saves highlights, which makes it difficult to determine their relative order.
+- For some books (seems to be non-DRM epubs, still experimenting with some formats), Kobo exports `.annot` files with annotations. These highlights will generally be exported in the order they appear in the book (highlights that are very close together may still be exported in the order they were created). These highlights also include a location number, which is calculated by multiplying the progress percentage they appear within the book (0-1) by 1000000000 (to account for many decimal places).   
+- Highlights not in the `.annot` format (e.g. Kobo DRM ebooks) will be exported in the order they were created rather than the order they appear in the book. This is due to the format Kobo saves highlights, which makes it difficult to determine their relative order. These do not include a page/location number.
 - All highlights will be exported every time, regardless of whether they've been exported before. Readwise will automatically detect duplicates and will not re-import them.
+
+## Changelog
+- 2021-01-17 - Import `.annot` files where possible to preserve order from book
 
 ## Todo
 - [ ] specify input and output via command line arguments
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import { open } from 'sqlite'
 import * as fs from 'fs';
 import * as path from 'path';
 import * as os from 'os';
+import convert from 'xml-js';
+import moment from 'moment';
 
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
@@ -14,6 +16,9 @@ const __dirname = dirname(__filename);
 const koboPath = '/Volumes/KOBOeReader';
 
 
+
+// First, grab highlights from the sqlite DB
+// This format is more annoying re: position in book, but more universal
 const db = await open({
     filename: path.join(koboPath, '.kobo','KoboReader.sqlite'),
     driver: sqlite3.Database
@@ -26,10 +31,7 @@ const bookmarks = await db.all(`SELECT b.Text as highlight, b.startContainerPath
     ORDER BY title, date
     `);
 
-const filename = path.join(__dirname, 'output.csv');
-const output = [];
 
-output.push(['Highlight','Title','Author','Note','Date'].join())
 
 function formatCsv(string){
     if(typeof string === 'string'){
@@ -39,14 +41,112 @@ function formatCsv(string){
     return string;
 }
 
+function makeId(title, author){
+    return `${title} ${author}`;
+}
+
+function formatDate(string){
+    return moment(string).format('YYYY-MM-DD hh:MM:ss')
+}
+
+const dbEntries = [];
+
 bookmarks.forEach(bookmark => {
     const row = [];
     row.push(formatCsv(bookmark.highlight));
     row.push(formatCsv(bookmark.title));
     row.push(formatCsv(bookmark.author));
     row.push(formatCsv(bookmark.note));
-    row.push(formatCsv(bookmark.date));
-    output.push(row.join());
+    row.push(formatCsv(formatDate(bookmark.date)));
+    row.push('');
+    dbEntries.push({
+        bookId: makeId(bookmark.title, bookmark.author),
+        row: row.join()
+    })
+    // output.push(row.join());
 });
+
+// now look for Digital Editions annotations
+// not all books store annotations in this format,
+// but those that do will be imported in the correct order
+
+const getAllFiles = function(dirPath, arrayOfFiles) {
+    const files = fs.readdirSync(dirPath)
+
+    arrayOfFiles = arrayOfFiles || []
+
+    files.forEach(function(file) {
+        if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+            arrayOfFiles = getAllFiles(path.join(dirPath, file), arrayOfFiles)
+        } else {
+            arrayOfFiles.push(path.join(dirPath, file))
+        }
+    })
+
+    return arrayOfFiles
+}
+
+const annotationFiles = getAllFiles(path.join(koboPath, 'Digital Editions')).filter(file => path.extname(file) === '.annot');
+
+const annotationFileBookIds = [];
+
+const annotationFileEntries = [];
+
+annotationFiles.forEach(annotationFile => {
+    var xml = fs.readFileSync(path.resolve(annotationFile), 'utf8');
+
+    var document = convert.xml2js(xml, {compact: true});
+
+    const info = document.annotationSet.publication;
+
+
+
+    const title = info['dc:title']._text;
+    const author = info['dc:creator']._text;
+
+    annotationFileBookIds.push(makeId(title, author));
+
+    const annotations = document.annotationSet.annotation;
+
+    annotations.forEach(annotation => {
+        const target = annotation.target.fragment;
+        const content = annotation.content;
+        const date = formatDate(annotation['dc:date']._text);
+        const location = Math.round(parseFloat(target._attributes.progress) * 1000000000);
+        const text = target.text ? target.text._text : "";
+        const comment = content ? content.text._text: "";
+        if(text){
+            const row = [];
+            row.push(formatCsv(text));
+            row.push(formatCsv(title));
+            row.push(formatCsv(author));
+            row.push(formatCsv(comment));
+            row.push(formatCsv(date));
+            row.push(formatCsv(location));
+
+            annotationFileEntries.push(row);
+        }
+    })
+})
+
+
+
+
+const filename = path.join(__dirname, 'output.csv');
+const output = [];
+
+output.push(['Highlight','Title','Author','Note','Date', 'Location'].join())
+
+dbEntries.forEach(entry => {
+    if(!annotationFileBookIds.includes(entry.bookId)){
+        output.push(entry.row);
+    }
+})
+
+annotationFileEntries.forEach(entry => {
+    output.push(entry.join());
+})
+
+
 
 fs.writeFileSync(filename, output.join(os.EOL));
